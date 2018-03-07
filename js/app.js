7046d3ab741ec31845c5ab0c2819be08a3d1578d
nattyayana <natnalezayana@gmail.com>
2018-03-06 22:56:23 -0800

    Created 2 of the 5 yes or no questions

---
+++ js/app.js
@@ -0,0 +1,22 @@
+'use strict';
+
+var shoeQuestion = parseInt(prompt('Do you think natty has more then 100 pairs of shoes? Please enter "yes" or "no"'));
+
+shoeQuestion = shoeQuestion.toLowerCase();
+
+if(shoeQuestion === 'no') {
+  alert('Your Right Natty Dose not lol If He Did He Would Give You A Pair!');
+} else {
+  alert('Nope! Natty Has About 40, Almost There');
+}
+
+var travelQuestion = parseInt(prompt('Do You Think Natty Has Left The United States Since He Came To American In 1997? Please enter "yes" or "no"'));
+
+travelQuestion = travelQuestion.toLowerCase();
+
+if(travelQuestion.toLowerCase() === 'no') {
+  alert('That Is Correct, I Need Stop Flying Only Domestically, And Get Out The Country ');
+} else {alert('I');
+}
+
+
